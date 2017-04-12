0edf162d745c135d26c454af010b82937f7fce02
Giampiero Specogna <giampi_12@hotmail.com>
2017-04-12 18:10:09 -0400

    Did the edit view and edit capabilities

--- src/BusinessDirectory/wwwroot/js/editBusinessController.js
+++ src/BusinessDirectory/wwwroot/js/editBusinessController.js
@@ -1,14 +1,39 @@
 ﻿//editBusinessController.js
 (function () {
     "use strict";
-    angular.module("publishBusiness-app")
+    angular.module("myBusiness-app")
             .controller("editBusinessController", editBusinessController);
     function editBusinessController($routeParams, $http) {
         var vm = this;
-        vm.Business = {};
+        vm.business = {};
+        vm.companyName = $routeParams.companyName;
         vm.isBusy = true;
         vm.errorMessage = "";
-
+        var url = "/api/business/" + vm.companyName;
+        $http.get(url)
+              .then(function (response) {
+                  angular.copy(response.data, vm.business);
+                  console.log(vm.business);
+              }, function (err) {
+                  vm.errorMessage = "Ocurrio un error al buscar el negocio "+err;
+              })
+        .finally(function () {
+            vm.isBusy = false;
+        });
+    
+        vm.editBusiness = function () {
+            vm.isBusy == true;
+            $http.post("/api/business/update", vm.business)
+                  .then(function (response) {
+                      alert("Negocio actualizado correctamente");
+                      window.open("#/", "_self");
+                  }, function (err) {
+                      vm.errorMessage = "Ocurrio un error al editar el negocio " + err;
+                  })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        }
 
     }
 })();
